9c4fef8694152e5695e0b12619a014d39a9f080a
lllloo <lllloo@users.noreply.github.com>
2024-04-29 02:02:43 +0000

    deploy: 2ce8d0ca212bb5ad0aa3a7ddafd1bb4ca824a299

--- static/DY7NyjPZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{c as t,o,a as c}from"./CwUu8pHG.js";const s={},n=c("h1",{class:"text-[24px]"},"Index",-1),r=[n];function a(_,d){return o(),t("div",null,r)}const l=e(s,[["render",a]]);export{l as default};
