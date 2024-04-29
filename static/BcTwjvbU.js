f1783dcdcc0b130672f9278f2ae29df4af4caa6b
lllloo <lllloo@users.noreply.github.com>
2024-04-29 09:48:14 +0000

    deploy: 6c4a031b8a463a8f15ce9da05cbcd82a26a86be9

---
+++ static/BcTwjvbU.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DlAUqK2U.js";import{c as t,o,a as c}from"./P0kSzoYu.js";const s={},n=c("h1",{class:"text-[24px]"},"Index",-1),r=[n];function a(_,d){return o(),t("div",null,r)}const l=e(s,[["render",a]]);export{l as default};
