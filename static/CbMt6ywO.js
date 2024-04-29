5acfef27cccf003c49e8fd95e7051d7b60e38c41
lllloo <lllloo@users.noreply.github.com>
2024-04-29 04:38:08 +0000

    deploy: 36ab6e6068b669829137e70fa9bd96ed7fb5902b

--- static/CbMt6ywO.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{c as t,o,a as c}from"./fpsC429_.js";const s={},n=c("h1",{class:"text-[24px]"},"Index",-1),r=[n];function a(_,d){return o(),t("div",null,r)}const l=e(s,[["render",a]]);export{l as default};
