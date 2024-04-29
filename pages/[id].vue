f74ac85b8719f104644df4ba4d7528a3b8b2703c
Barney <muki.barney@gmail.com>
2024-04-29 16:56:09 +0800

    feat: decodeURIComponent

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,12 +1,18 @@
 <script setup>
+import { computed } from 'vue';
+
 const route = useRoute()
 console.log(route)
+
+const product = computed(() => {
+  return decodeURIComponent(route.path.replace('/',''))
+})
 
 </script>
 <template>
   <div>
     <h1 class="text-[20px]">
-      商品 {{ route.path.replace('/','') }}
+      商品: {{ product }}
     </h1>
   </div>
 </template>
