6206f3415d6e9723b2ea0c5140e65b9a8c3d9e4a
lllloo <lllloo@users.noreply.github.com>
2024-05-03 09:24:09 +0000

    deploy: 6db3505f37ffb301aff621ca630f3df731d121a7

--- static/1OwLZjjK.js
+++
@@ -1,1 +0,0 @@
-import{u as t,c as a,t as s,q as o,o as r}from"./BDIBNfYb.js";const _={__name:"[id]",setup(n){const e=t();return(c,u)=>(r(),a("div",null," page: "+s(o(e).params.id),1))}};export{_ as default};
